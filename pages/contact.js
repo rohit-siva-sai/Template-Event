bb22d8d01d791571c2fab343d478726f344dbaf9
rohit-siva-sai <106007387+rohit-siva-sai@users.noreply.github.com>
2023-02-24 17:41:19 +0530

    initial commit

--- pages/contact.js
+++ pages/contact.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 
 const Contact = () => {
   return (
@@ -9,14 +9,16 @@ const Contact = () => {
             Contact
           </h1>
         </div>
-     <div>
-          <h1 className="font-bold font-serif text-6xl text-white text-center">
-            Contact
-          </h1>
-        </div>
+        
+       
+        <div></div>
       </div>
+        <div className="p-4 md:p-8 text-red-600" >
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque non, libero maiores vitae cupiditate atque nulla corrupti veritatis mollitia ex necessitatibus odit molestiae deserunt, voluptatibus eum, architecto officiis. Vitae, ipsa!
+          At necessitatibus cupiditate culpa doloribus harum deleniti facilis, itaque ad ut odit explicabo nulla voluptatum rerum praesentium deserunt. Iste, consequuntur iusto perspiciatis perferendis quidem dolor. Dolore, veritatis delectus. Mollitia, itaque.
+        </div>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
